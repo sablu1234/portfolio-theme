1414f6754b2e399c9ac796dcfb56e3cb0e612c03
sablu1234 <150502592+sablu1234@users.noreply.github.com>
2024-06-30 15:52:00 +0600

    themefile 5

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+// $total_posts = wp_count_posts()->publish;
+// echo 'total publish post '.$total_posts;
+
+
+// echo wp_count_posts()->publish;
+echo wp_count_posts()->publish;
+
+// $postcount= total_posts ;
+// while($postcount<4){
+//     echo $postcount ."<br>";
+//     $postcount++;
+// }
+
+
+?>
