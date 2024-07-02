b85a5776372e8f62a121bf9045be5cd43a0eea1b
sablu1234 <150502592+sablu1234@users.noreply.github.com>
2024-07-02 17:57:05 +0600

    portfolio-theme

---
+++ function.php
@@ -0,0 +1,21 @@
+<?php
+
+// $post_num= wp_count_posts()->publish;
+// echo $post_num."<br>";
+
+
+// $postcount =$post_num;
+// while($postcount<4){
+//     echo $postcount."<br>";
+//     $postcount++;
+// }
+
+
+$postcount =0;
+while($postcount<4){
+    echo $postcount."<br>";
+    $postcount++;
+}
+    
+
+?>
